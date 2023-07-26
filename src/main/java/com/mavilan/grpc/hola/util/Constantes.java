f97e7944ff8ddbd0c9bd4499db187a8935f4dbfd
Marco Avila <maavila@minsait.com>
2023-07-26 11:58:32 -0600

    sonar

--- src/main/java/com/mavilan/grpc/hola/util/Constantes.java
+++ src/main/java/com/mavilan/grpc/hola/util/Constantes.java
@@ -1,6 +1,8 @@
 package com.mavilan.grpc.hola.util;
 
 public class Constantes {
+
+    private Constantes(){}
     public static final int PORT = 50050; //puerto del server
     public static final String HOSTNAME = "localhost"; //hostname del server
 
